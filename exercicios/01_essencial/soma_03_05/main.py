6545ccb6c76530f67faefaa3e33a1fafc0d45b67
Maria Duque <maria.oliveira.duque@gmail.com>
2026-03-10 12:20:35 -0300

    Adiciona resolução dos exercícios

--- exercicios/01_essencial/soma_03_05/main.py
+++ exercicios/01_essencial/soma_03_05/main.py
@@ -1,7 +1,12 @@
+
 def resposta(n):
     soma = 0
 
     for i in range(n):
-        if i % 3 == 0 or i % 5 == 0:
+        divisivel_3 = i % 3 == 0
+        divisivel_5 = i % 5 == 0
+
+        if divisivel_3 or divisivel_5:
             soma += i
+
     return soma
